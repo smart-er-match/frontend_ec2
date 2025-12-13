2bbb105beec19585de8b437ebecac7a1d0257e4c
taek-99 <gunha1215@gmail.com>
2025-12-13 10:56:00 +0000

    모바일 ui 깨짐 해결 완료

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -6,7 +6,7 @@
   <div>
     <Navbar/>
     <div class="min-h-screen flex justify-center items-center bg-slate-100">
-    <div class="w-full max-w-xl px-4 min-w-[460px]">
+    <div class="w-full max-w-xl px-4 w-full">
       <div class="bg-white rounded-xl shadow-md p-8">
         <router-view />
       </div>
